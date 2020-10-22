e8a7732e1a47616147e3c6967bed6659c9e4deb0
Amrit-Code <59258098+Amrit-Code@users.noreply.github.com>
2020-10-22 18:27:27 +0100

    Committtt
    asdf

---
+++ Assets/Scripts/Player/MoveAnimation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAnimation : MonoBehaviour
+{
+
+    Animator animPlayer;
+    bool walking = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animPlayer = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartAnimation(){
+        if(walking == false){
+            animPlayer.SetTrigger("walk");
+            walking = true;
+        }
+        
+    }
+
+    public void StopAnimation(){
+        if(walking == true){
+            animPlayer.SetTrigger("stop");
+            walking = false;
+        }
+    }
+}

4e5846f67246df907483ce2fe073ab4d9b9dc7d4
Amrit-Code <59258098+Amrit-Code@users.noreply.github.com>
2020-10-28 14:05:40 +0000

    Updated camera controlls and light for player

---
+++ Assets/Scripts/Lights/LightController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject playerHead;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerHead.transform.position + new Vector3(0,1f,0);
+        transform.rotation = playerHead.transform.rotation;
+    }
+}

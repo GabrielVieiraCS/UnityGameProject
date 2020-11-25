fedeca96637dc3f6a4707597e68e3a9f4a75442a
Amrit-Code <59258098+Amrit-Code@users.noreply.github.com>
2020-11-25 14:14:25 +0000

    lights

---
+++ Assets/Scripts/Lights/MapLightController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLightController : MonoBehaviour
+{
+
+    [Header("Time unit:")]
+    public float unit;
+    
+    private GameObject[] normalLights;
+    private GameObject morseLight;
+    private string morsePattern = ".- .--- --.";
+
+    void Start()
+    {
+        normalLights = GameObject.FindGameObjectsWithTag("Light");
+        morseLight = GameObject.Find("MorseSpotlight");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

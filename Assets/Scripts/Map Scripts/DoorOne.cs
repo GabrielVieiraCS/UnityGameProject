64281354782e4ce586929094b3022d665dca4de9
Amrit-Code <59258098+Amrit-Code@users.noreply.github.com>
2020-11-25 18:57:27 +0000

    Small UI Update

---
+++ Assets/Scripts/Map Scripts/DoorOne.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorOne : MonoBehaviour
+{
+    
+    private GameObject player;
+    private Interaction interact;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        interact = player.GetComponent<Interaction>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    private void CheckIfNear(){
+        float distance = (transform.position - player.transform.position).magnitude;
+        if(distance < 3f){
+            interact.NearObjective(1);
+        }
+    }
+}
---
+++ Assets/Scripts/Player/Interaction.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interaction : MonoBehaviour
+{
+    [Header("Use Text")]
+    public Text useText;
+
+    private int nearID = 0; // ) means no Obj near
+    
+    
+    void Start()
+    {
+        useText.CrossFadeAlpha(0.3f, 0f, false);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    public void NearObjective(int objID){
+        nearID = objID;
+        
+    }
+}

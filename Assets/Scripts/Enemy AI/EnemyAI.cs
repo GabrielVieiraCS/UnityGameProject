0842504ecc189c74d2835056cd4fd12a6121546b
Amrit-Code <59258098+Amrit-Code@users.noreply.github.com>
2020-11-30 15:43:40 +0000

    Gabs Monster Update

---
+++ Assets/Scripts/Enemy AI/EnemyAI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent Enemy;
+    public GameObject Player;
+    public float EnemyDistanceRun = 4.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Enemy = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        // Run towards Player
+
+        if (distance < EnemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            Enemy.SetDestination(newPos);
+        }
+    }
+}

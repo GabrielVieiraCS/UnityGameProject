bf5a74f6b54c646a5566938618515723262e7225
Amrit-Code <59258098+Amrit-Code@users.noreply.github.com>
2020-12-05 16:04:48 +0000

    main menu and sound fix

---
+++ Assets/Scripts/Others/MainMenuHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuHandler : MonoBehaviour
+{
+    public GameObject mainCanvas;
+    public GameObject rulesCanvas;
+
+    void Start(){
+        rulesCanvas.SetActive(false);
+    }
+
+    public void DisplayRules(){
+        rulesCanvas.SetActive(true);
+        mainCanvas.SetActive(false);
+    }
+
+    public void StartGame(){
+        SceneManager.LoadScene("MainLevel");
+    }
+}
